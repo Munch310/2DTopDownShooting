ad042131dee5031ac417d151a0d26bf8e0e73023
Munch310 <ghkdlsi@naver.com>
2023-09-18 15:47:53 +0900

    [ADD] Animations
    - Player에 Animations를 추가헀습니다.
    
    1. player_idle
    2. player_run
    3. player_attack
    4. player_hit

---
+++ Assets/Scripts/Controllers/TopDownAnimaionController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAnimaionController : TopDownAnimations
+{
+    // 해싱하는 이유? 키 값을 스트링으로 제공하면, 스트링을 비교하는 연산이 일어난다. 문자열을 비교하는 연산은 비용이 높다.
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int IsHit = Animator.StringToHash("IsHit");
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+     void Start()
+     {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += Move;
+     }
+
+    private void Move(Vector2 obj)
+    {
+        animator.SetBool(IsWalking, obj.magnitude > .5f);
+    }
+
+    private void Attacking(AttackSO sO)
+    {
+        animator.SetTrigger(Attack);
+    }
+
+    private void Hit()
+    {
+        animator.SetBool(IsHit, true);
+    }
+
+    private void InvinciblityEnd()
+    {
+        animator.SetBool(IsHit, false);
+    }
+}
